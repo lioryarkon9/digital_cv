affad442af7437e279d369fc22ba39eeaf6638bd
leo <lior_yarkon@hotmail.com>
2019-07-07 20:56:25 +0300

    work in progress

--- react-client/src/Root.js
+++ react-client/src/Root.js
@@ -3,6 +3,8 @@ import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {setAppView} from './actions/appSettings';
 import HomeView from './views/Home';
+import CourseCartApp from './views/CourseCart';
+import {URL_COURSE_CART_VIEW} from './consts';
 
 
 class Root extends React.Component {
@@ -14,6 +16,10 @@ class Root extends React.Component {
             <Router>
                 <Switch>
                     <Route exact path={'/'} component={HomeView}/>
+                    <Route 
+                        path={URL_COURSE_CART_VIEW.slice(1, URL_COURSE_CART_VIEW.length)}
+                        component={CourseCartApp}
+                    />
                     <Route path={'*'} component={(browserStuff) => <h2>Error: Page Not Found</h2>}/>
                 </Switch>
             </Router>
---
+++ react-client/src/views/CourseCart/CourseCart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AppWrapper from '../AppWrapper';
+
+
+
+class CourseCartApp extends React.Component {
+    render () {
+        return (
+            <AppWrapper>
+                <div>
+                    CourseCartApp
+                </div>
+            </AppWrapper>
+        );
+    }
+}
+
+
+export default CourseCartApp;
